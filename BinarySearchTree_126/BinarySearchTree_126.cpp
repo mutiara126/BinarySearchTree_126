18d741af89a2c33a06e44860a94dea1ea4d2213e
mutiara126 <salsamutiara1812@gmail.com>
2023-06-30 12:18:12 +0800

    membuat new node menjadi root

--- BinarySearchTree_126/BinarySearchTree_126.cpp
+++ BinarySearchTree_126/BinarySearchTree_126.cpp
@@ -36,5 +36,15 @@ public:
         newNode->rightchild = NULL; // Make the right child of the new node point to null
 
         Node* parent = NULL;
+        Node* currentNode = NULL;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+    
+    if (parent == NULL) // If the parent is NULL (Tree is empty)
+    {
+        ROOT = newNode; // Mark the new node as ROOT
+       
+    }
     }
 };
+
+ 
